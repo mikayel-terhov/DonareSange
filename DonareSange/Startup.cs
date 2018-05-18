dc881d635d2fd226bffaa63e7e8046e1217e45ea
mikayel-terhov <michael.terhovhannisyan@gmail.com>
2018-05-18 15:06:20 +0300

    Add project files.

---
+++ DonareSange/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DonareSange.Startup))]
+namespace DonareSange
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
